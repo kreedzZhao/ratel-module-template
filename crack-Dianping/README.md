973241bd07450cccf1221b7bcf694fface9762d7
kreedz <luserkaiz@gmail.com>
2023-09-01 00:31:36 +0800

    feat: dp day 1

---
+++ crack-Dianping/README.md
@@ -0,0 +1,39 @@
+- url hook
+```text
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.fork.b.a(RxForkHttpService.java:182)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.fork.b.exec(RxForkHttpService.java:102)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m.a(OnSubscribeWithCache.java:190)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m.b(OnSubscribeWithCache.java:43)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m$b.a(OnSubscribeWithCache.java:460)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.preload.engine.fetch.d.a(FetchPreloadInterceptor.kt:104)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.preload.engine.fetch.d.intercept(FetchPreloadInterceptor.kt:34)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m$b.a(OnSubscribeWithCache.java:452)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.s.intercept(RxNVNetworkMockInterceptor.java:39)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m$b.a(OnSubscribeWithCache.java:452)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.dataservice.mapi.impl.d.intercept(MapiInterceptor.java:53)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m$b.a(OnSubscribeWithCache.java:452)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.meituan.metrics.traffic.shark.SharkRxInterceptor.intercept(SharkRxInterceptor.java:65)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m$b.a(OnSubscribeWithCache.java:452)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m.a(OnSubscribeWithCache.java:137)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at com.dianping.nvnetwork.m.call(OnSubscribeWithCache.java:43)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at rx.internal.operators.k.a(OnSubscribeLift.java:50)
+08-31 22:24:54.148 17432 17650 I DP_HOOK : 	at rx.internal.operators.k.call(OnSubscribeLift.java:30)
+```
+- com.dianping.nvnetwork.Request 构造函数
+```text
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.nvnetwork.Request$Builder.build(Request.java:596)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.dataservice.mapi.impl.DefaultMApiService.transferRequest(DefaultMApiService.java:267)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.dataservice.mapi.impl.DefaultMApiService.exec(DefaultMApiService.java:301)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.dataservice.mapi.impl.DefaultMApiService.exec(DefaultMApiService.java:53)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.preload.commons.network.c.a(Mapi.kt:131)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.preload.engine.fetch.b.a(FetchPreloadEngine.kt:175)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.preload.bridge.PreloadRequestBridge.doPreloadRequest(PreloadRequestBridge.kt:47)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at java.lang.reflect.Method.invoke(Native Method)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.picassocontroller.annotation.c.a(PicassoModuleUtil.java:268)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.picassocontroller.annotation.c.a(PicassoModuleUtil.java:241)
+08-31 22:47:08.952 19633 19929 D DP_HOOK : 	at com.dianping.picassocontroller.bridge.a.exec(PCSBImpl.java:33)
+```
+- com.dianping.nvnetwork.NVDefaultNetworkService#exec(com.dianping.nvnetwork.Request, com.dianping.nvnetwork.o)
+- com.dianping.dataservice.mapi.impl.DefaultMApiService.a#onRequestFinish 返回值
+- 另一个异步解密 com.dianping.dataservice.mapi.impl.DefaultMApiService.a 第三个参数fVal2 就是
+- com.dianping.picasso.commonbridge.MapiModule$3
---
+++ crack-Dianping/src/main/java/ratel/com/dianping/v1/HookEntry.java
@@ -0,0 +1,159 @@
+package ratel.com.dianping.v1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+
+import com.virjar.ratel.api.RatelToolKit;
+import com.virjar.ratel.api.inspect.ForceFiledViewer;
+import com.virjar.ratel.api.rposed.IRposedHookLoadPackage;
+import com.virjar.ratel.api.rposed.RC_MethodHook;
+import com.virjar.ratel.api.rposed.RposedBridge;
+import com.virjar.ratel.api.rposed.RposedHelpers;
+import com.virjar.ratel.api.rposed.callbacks.RC_LoadPackage;
+
+import external.com.alibaba.fastjson.JSONObject;
+
+import java.net.URL;
+
+import cn.iinti.sekiro3.business.api.SekiroClient;
+import cn.iinti.sekiro3.business.api.interfaze.ActionHandler;
+import cn.iinti.sekiro3.business.api.interfaze.SekiroRequest;
+import cn.iinti.sekiro3.business.api.interfaze.SekiroResponse;
+
+/**
+ * Created by virjar on 2018/10/6.
+ */
+
+public class HookEntry implements IRposedHookLoadPackage {
+    private static final String tag = "DP_HOOK";
+
+
+    @Override
+    public void handleLoadPackage(final RC_LoadPackage.LoadPackageParam lpparam) {
+
+        addFloatingButtonForActivity(lpparam);
+        Log.i(tag, "hook end");
+
+        startSekiro(lpparam);
+
+//        RposedBridge.hookAllConstructors(URL.class, new RC_MethodHook() {
+//            @Override
+//            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                String url = param.thisObject + "";
+//                Log.i(tag, "access url:" + url);
+//                if (url.contains("shop")) {
+//                    Log.i(tag, "hint url:", new Throwable());
+//                }
+//            }
+//        });
+        // com.dianping.nvnetwork.Request
+//        RposedBridge.hookAllConstructors(
+//                RposedHelpers.findClass("com.dianping.nvnetwork.Request", lpparam.classLoader),
+//                new RC_MethodHook() {
+//                    @Override
+//                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                        super.beforeHookedMethod(param);
+//                        Log.d(tag, "request constructor: " + JSONObject.toJSONString(param.args), new Throwable());
+//                    }
+//                }
+//        );
+        // com.dianping.nvnetwork.NVDefaultNetworkService#exec(com.dianping.nvnetwork.Request, com.dianping.nvnetwork.o)
+//        RposedBridge.hookAllMethods(
+//                RposedHelpers.findClass("com.dianping.nvnetwork.NVDefaultNetworkService", lpparam.classLoader),
+//                "exec",
+//                new RC_MethodHook() {
+//                    @Override
+//                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                        super.beforeHookedMethod(param);
+//                        Log.d(tag, "NVDefaltNetworkService exec arg1: "+ JSONObject.toJSONString(ForceFiledViewer.toView(param.args[0])));
+//                        Log.d(tag, "NVDefaltNetworkService exec arg2: "+ JSONObject.toJSONString(param.args[1]));
+//                        Log.d(tag, "arg2 type: "+ param.args[1].getClass());
+//                    }
+//                }
+//        );
+//        RposedHelpers.findAndHookMethod(
+//                "com.dianping.dataservice.mapi.impl.DefaultMApiService.a",
+//                lpparam.classLoader,
+//                "onRequestFinish",
+//                RposedHelpers.findClass("com.dianping.nvnetwork.Request", lpparam.classLoader),
+//                RposedHelpers.findClass("com.dianping.nvnetwork.q", lpparam.classLoader),
+//                new RC_MethodHook() {
+//                    @Override
+//                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                        super.beforeHookedMethod(param);
+//                        Log.d(tag, "resposne: " + JSONObject.toJSONString(ForceFiledViewer.toView(param.args[1])));
+//                    }
+//                }
+//        );
+
+        RposedBridge.hookAllConstructors(
+                RposedHelpers.findClass("com.dianping.dataservice.mapi.impl.DefaultMApiService.a", lpparam.classLoader),
+                new RC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        Log.d(tag, "enter DefaultMapi");
+                        if (param.args[3] != null){
+                            Log.d(tag, "decrypt class: "+ param.args[3].getClass());
+                        }
+                    }
+                }
+        );
+
+    }
+
+    private static void startSekiro(RC_LoadPackage.LoadPackageParam lpparam) {
+        new SekiroClient("test", "testClient", "192.168.1.13", 5612)
+                .setupSekiroRequestInitializer((sekiroRequest, handlerRegistry) ->
+                        // 注册一个接口，名为testAction
+                        handlerRegistry.registerSekiroHandler(new ActionHandler() {
+                            @Override
+                            public String action() {
+                                return "testAction";
+                            }
+
+                            @Override
+                            public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
+                                // 接口处理逻辑，我们不做任何处理，直接返回字符串：ok
+                                Log.d(tag, "sekiro received. ");
+                                sekiroResponse.success("ok");
+                            }
+                        })
+                ).start();
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addFloatingButtonForActivity(final RC_LoadPackage.LoadPackageParam lpparam) {
+        RposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new RC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
+                new Handler(Looper.getMainLooper())
+                        .postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                createAndAttachFloatingButtonOnActivity((Activity) param.thisObject);
+                            }
+                        }, 1000);
+            }
+
+            private void createAndAttachFloatingButtonOnActivity(Activity activity) {
+                Context context = RatelToolKit.ratelResourceInterface.createContext(lpparam.modulePath, HookEntry.class.getClassLoader(), RatelToolKit.sContext);
+
+                FrameLayout frameLayout = (FrameLayout) activity.getWindow().getDecorView();
+                LayoutInflater.from(context).cloneInContext(context)
+                        .inflate(R.layout.float_button, frameLayout);
+
+            }
+        });
+    }
+}
