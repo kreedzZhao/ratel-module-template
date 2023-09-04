d77f641ea5bd02bc549872095c43407706ef5a84
kreedz <luserkaiz@gmail.com>
2023-09-05 00:23:57 +0800

    feat: bd day 2

--- crack-bdmap/src/main/java/ratel/crack/com/baidu/BaiduMap/HookEntry.java
+++ crack-bdmap/src/main/java/ratel/crack/com/baidu/BaiduMap/HookEntry.java
@@ -9,9 +9,11 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.virjar.ratel.api.RatelToolKit;
+import com.virjar.ratel.api.inspect.ClassLoadMonitor;
 import com.virjar.ratel.api.inspect.ForceFiledViewer;
 import com.virjar.ratel.api.rposed.IRposedHookLoadPackage;
 import com.virjar.ratel.api.rposed.RC_MethodHook;
@@ -31,29 +33,89 @@ public class HookEntry implements IRposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(final RC_LoadPackage.LoadPackageParam lpparam) {
-
-
-        // 关键类 com.baidu.platform.comapi.newsearch.SearcherImpl.getPBResultInternal
-        // 但是这个往上追溯是一个新的线程，所以在他当前的文件中，一般解析和请求会在一个文件里
-        // sendRequest(com.baidu.platform.comapi.newsearch.SearchRequest searchRequest)
-        // com.baidu.platform.comapi.newsearch.SearcherImpl#sendRequest
-        RposedHelpers.findAndHookMethod(
-                "com.baidu.platform.comapi.newsearch.SearcherImpl",
-                lpparam.classLoader,
-                "sendRequest",
-                RposedHelpers.findClass("com.baidu.platform.comapi.newsearch.SearchRequest", lpparam.classLoader),
-                new RC_MethodHook() {
+        // 其实任一事件进去之后，就可以看出来 com.baidu.titan.sdk.runtime.Interceptable
+        // 实现了一个全局的匿名类 $ic 开始是反射生成的
+        /*
+         this.mLoadFuture = newSingleThreadExecutor.submit(new java.util.concurrent.Callable<java.lang.Integer>() {
+            public java.lang.Integer call() throws java.lang.Exception {
+                int access$000 = com.baidu.titan.sdk.loader.LoaderManager.this.loadPatch(false, patchInstallInfo);
+                com.baidu.titan.sdk.loader.LoaderManager.this.mLoadState = access$000;
+                return java.lang.Integer.valueOf(access$000);
+            }
+        });
+         */
+        // com.baidu.titan.sdk.pm.PatchInstallInfo#getOrderedDexList
+        ClassLoadMonitor.addClassLoadMonitor(
+                "com.baidu.titan.sdk.pm.TitanPaths",
+                new ClassLoadMonitor.OnClassLoader() {
                     @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        super.beforeHookedMethod(param);
-                        Log.i(TAG, "sendRequest: "+JSONObject.toJSONString(param.args[0]), new Throwable());
+                    public void onClassLoad(Class<?> clazz) {
+                        Log.d(TAG, "TitanPaths loaded");
+                        // com.baidu.titan.sdk.pm.TitanPaths#getPatchDir
+                        RposedHelpers.findAndHookMethod(
+                                "com.baidu.titan.sdk.pm.TitanPaths",
+                                lpparam.classLoader,
+                                "getPatchDir",
+                                String.class,
+                                new RC_MethodHook() {
+                                    @Override
+                                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                        super.afterHookedMethod(param);
+                                        Log.i(TAG, "path dir: "+param.getResult().toString());
+                                    }
+                                }
+                        );
                     }
                 }
         );
 
 
+
         addFloatingButtonForActivity(lpparam);
         Log.i(TAG, "hook end");
+    }
+
+    public void heatMap(RC_LoadPackage.LoadPackageParam lpparam){
+        // com.baidu.mapframework.common.mapview.HeatMapView#onClicked
+        RposedBridge.hookAllMethods(
+                RposedHelpers.findClass("com.baidu.mapframework.common.mapview.HeatMapView", lpparam.classLoader),
+                "onClicked",
+                new RC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        Log.i(TAG, "onclicked is called ", new Throwable());
+                    }
+                }
+        );
+        // com.baidu.platform.comapi.util.BMEventBus#doEvent
+        RposedBridge.hookAllMethods(
+                RposedHelpers.findClass("com.baidu.platform.comapi.util.BMEventBus", lpparam.classLoader),
+                "doEvent",
+                new RC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        // com.baidu.platform.comapi.util.BMEventBus$ModuleOnEvent
+                        Log.i(TAG, "BMEventBus doEvent param1 type: "+param.args[0]);
+                        Log.i(TAG, "BMEventBus doEvent param2 type: "+param.args[1], new Throwable());
+                    }
+                }
+        );
+        // 所有的事件存放在一个 hashmap 中
+        // com.baidu.platform.comapi.util.BMEventBus#regist(com.baidu.platform.comapi.util.BMEventBus.OnEvent, java.lang.Object, java.lang.Class<?>, java.lang.Class<?>...)
+        // 当前 注册了所有重要的事件，例如安全相关 com.baidu.baidumaps.secure.g
+        RposedBridge.hookAllMethods(
+                RposedHelpers.findClass("com.baidu.platform.comapi.util.BMEventBus", lpparam.classLoader),
+                "regist",
+                new RC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        Log.i(TAG, "regist param1 type: "+param.args[0]);
+                    }
+                }
+        );
     }
 
 
@@ -105,6 +167,25 @@ public class HookEntry implements IRposedHookLoadPackage {
                     }
                 }
         );
+        // 关键类 com.baidu.platform.comapi.newsearch.SearcherImpl.getPBResultInternal
+        // 但是这个往上追溯是一个新的线程，所以在他当前的文件中，一般解析和请求会在一个文件里
+        // sendRequest(com.baidu.platform.comapi.newsearch.SearchRequest searchRequest)
+        // com.baidu.platform.comapi.newsearch.SearcherImpl#sendRequest
+        RposedHelpers.findAndHookMethod(
+                "com.baidu.platform.comapi.newsearch.SearcherImpl",
+                lpparam.classLoader,
+                "sendRequest",
+                RposedHelpers.findClass("com.baidu.platform.comapi.newsearch.SearchRequest", lpparam.classLoader),
+                new RC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        Log.i(TAG, "sendRequest: "+JSONObject.toJSONString(param.args[0]), new Throwable());
+                    }
+                }
+        );
+
+        // com.baidu.mapframework.searchcontrol.baseline.SearchBaseLineImpl#sendRequest
 
     }
 
