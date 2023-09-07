d358116f10ee6f3083099e1a80fe5ff1508e89f3
kreedz <luserkaiz@gmail.com>
2023-09-07 23:55:36 +0800

    feat: bd day 6

---
+++ crack-gdmap/src/main/java/ratel/crack/com/autonavi/minimap/HookEntry.java
@@ -0,0 +1,108 @@
+package ratel.crack.com.autonavi.minimap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Parcel;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+
+import com.virjar.ratel.api.RatelToolKit;
+import com.virjar.ratel.api.inspect.ForceFiledViewer;
+import com.virjar.ratel.api.rposed.IRposedHookLoadPackage;
+import com.virjar.ratel.api.rposed.RC_MethodHook;
+import com.virjar.ratel.api.rposed.RposedBridge;
+import com.virjar.ratel.api.rposed.RposedHelpers;
+import com.virjar.ratel.api.rposed.callbacks.RC_LoadPackage;
+
+import java.net.URL;
+
+import external.com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by virjar on 2018/10/6.
+ */
+
+public class HookEntry implements IRposedHookLoadPackage {
+    private static final String TAG = "GD_HOOK";
+
+    public static void loge(String tag, String msg) {
+        if (tag == null || tag.length() == 0 || msg == null || msg.length() == 0) {
+            return;
+        }
+        int segmentSize = 3 * 1024;
+        long length = msg.length();
+        if (length <= segmentSize) {// 长度小于等于限制直接打印
+            Log.e(tag, msg);
+        } else {
+            while (msg.length() > segmentSize) {// 循环分段打印日志
+                String logContent = msg.substring(0, segmentSize);
+                msg = msg.replace(logContent, "");
+                Log.i(tag, logContent);
+            }
+            Log.i(tag, msg);// 打印剩余日志
+        }
+
+    }
+
+
+    @Override
+    public void handleLoadPackage(final RC_LoadPackage.LoadPackageParam lpparam) {
+//        RposedBridge.hookAllConstructors(URL.class, new RC_MethodHook() {
+//            @Override
+//            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                String url = param.thisObject + "";
+////                Log.i(TAG, "access url:" + url);
+//                if (url.contains("search_poi/search")) {
+//                    Log.i(TAG, "hint url:", new Throwable());
+//                }
+//            }
+//        });
+
+        RposedHelpers.findAndHookMethod(Intent.class, "writeToParcel", Parcel.class, int.class,
+                new RC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        Intent intent = (Intent) param.thisObject;
+                        intent.getStringExtra("test");
+                        String sss = JSONObject.toJSONString(ForceFiledViewer.toView(intent));
+                        loge("Result", sss);
+                        Log.i(TAG, "intent writeToParcel: " + sss);
+                        Log.i(TAG, "intent writeToParcel trace:", new Throwable());
+                    }
+                });
+
+
+        addFloatingButtonForActivity(lpparam);
+        Log.i(TAG, "hook end");
+    }
+
+
+    private static void addFloatingButtonForActivity(final RC_LoadPackage.LoadPackageParam lpparam) {
+        RposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new RC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
+                new Handler(Looper.getMainLooper())
+                        .postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                createAndAttachFloatingButtonOnActivity((Activity) param.thisObject);
+                            }
+                        }, 1000);
+            }
+
+            private void createAndAttachFloatingButtonOnActivity(Activity activity) {
+                Context context = RatelToolKit.ratelResourceInterface.createContext(lpparam.modulePath, HookEntry.class.getClassLoader(), RatelToolKit.sContext);
+
+                FrameLayout frameLayout = (FrameLayout) activity.getWindow().getDecorView();
+                LayoutInflater.from(context).cloneInContext(context)
+                        .inflate(R.layout.float_button, frameLayout);
+
+            }
+        });
+    }
+}
